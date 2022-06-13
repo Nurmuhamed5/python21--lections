8252c15a1c2e24fa267bbcf5c9b7a33da2d91e9a
MBA <mba@MacBook-Air-MBA.local>
2022-06-13 17:55:08 +0600

    calc_.py

---
+++ basics/calc_.py
@@ -0,0 +1,19 @@
+a = float(input('Введите первое число: '))
+b = float(input('Введите первое число: '))
+c = input('Выберите операцию из следующих:' '+' '-' '*' '/' '%' '**' '//')
+
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '*':
+    print(a * b)
+elif c == '/':
+    print(a / b)
+elif c == '%':
+    print(a % b)
+elif c == '**':
+    print(a ** b)
+elif c == '//':
+    print(a // b)
+    
